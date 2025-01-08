ebd30d409f2d319cfb699984c6ef66354ade515f
andrei.balica <andrei.balica@stud.acs.upb.ro>
2025-01-08 14:40:05 +0200

    third commit

--- src/cmd.c
+++ src/cmd.c
@@ -32,7 +32,7 @@ void file_descriptor(char *src, int dst, int flags) {
 
 void both_redirections(word_t *out_err) {
   if (out_err == NULL)
-	 return;
+    return;
   char *file = get_word(out_err);
   int flags = O_WRONLY | O_CREAT | O_TRUNC;
 
@@ -82,7 +82,7 @@ void error_redirection(word_t *err, int io_flags) {
 }
 
 void command_redirections(simple_command_t *s) {
-  if (s->err && s->out && s->err == s->out) {
+  if (s->err && s->out) {
     both_redirections(s->out);
   } else {
     input_redirection(s->in);
@@ -95,8 +95,9 @@ void command_redirections(simple_command_t *s) {
  * Internal change-directory command.
  */
 static bool shell_cd(word_t *dir) {
-   char *path = get_word(dir);
-   return chdir(path);
+  /* TODO: Execute cd. */
+  char *path = get_word(dir);
+  return chdir(path);
 }
 
 /**
@@ -112,6 +113,7 @@ static int shell_exit(void) {
  * external command).
  */
 static int parse_simple(simple_command_t *s, int level, command_t *father) {
+
   /* TODO: Sanity checks. */
 
   if (s == NULL)
@@ -128,23 +130,22 @@ static int parse_simple(simple_command_t *s, int level, command_t *father) {
   if (strcmp(cmd_name, "cd") == 0) {
     int saved_stdout = dup(STDOUT_FILENO);
     int saved_stderr = dup(STDERR_FILENO);
-        
+
     command_redirections(s);
     int ret = shell_cd(s->params);
-        
+
     dup2(saved_stdout, STDOUT_FILENO);
     dup2(saved_stderr, STDERR_FILENO);
     close(saved_stdout);
     close(saved_stderr);
-        
+
     free(cmd_name);
     return ret;
   }
-  if (strcmp(cmd_name, "exit") == 0) {
+  if (strcmp(cmd_name, "exit") == 0 || strcmp(cmd_name, "quit") == 0) {
     free(cmd_name);
     return shell_exit();
   }
-  
 
   /* TODO: If variable assignment, execute the assignment and return
    * the exit status.
@@ -168,19 +169,32 @@ static int parse_simple(simple_command_t *s, int level, command_t *father) {
   pid_t pid = fork();
 
   if (pid < 0) {
+    for (int i = 0; i < size; i++) {
+      free(argv[i]);
+    }
     free(argv);
+    free(cmd_name);
     return -1;
   }
 
   if (pid == 0) {
     command_redirections(s);
     execvp(argv[0], argv);
+    fprintf(stderr, "Execution failed for '%s'\n", argv[0]);
+    for (int i = 0; i < size; i++) {
+      free(argv[i]);
+    }
+    free(argv);
+    free(cmd_name);
     exit(1);
   }
 
   int status;
   waitpid(pid, &status, 0);
 
+  for (int i = 0; i < size; i++) {
+    free(argv[i]);
+  }
   free(argv);
   free(cmd_name);
   return WEXITSTATUS(status);
@@ -193,7 +207,31 @@ static bool run_in_parallel(command_t *cmd1, command_t *cmd2, int level,
                             command_t *father) {
   /* TODO: Execute cmd1 and cmd2 simultaneously. */
 
-  return true; /* TODO: Replace with actual exit status. */
+  pid_t pid1 = fork();
+  if (pid1 < 0) {
+    return false;
+  }
+
+  if (pid1 == 0) {
+    int exit_status = parse_command(cmd1, level + 1, father);
+    exit(exit_status);
+  }
+
+  pid_t pid2 = fork();
+  if (pid2 < 0) {
+    return false;
+  }
+
+  if (pid2 == 0) {
+    int exit_status = parse_command(cmd2, level + 1, father);
+    exit(exit_status);
+  }
+
+  int status1, status2;
+  waitpid(pid1, &status1, 0);
+  waitpid(pid2, &status2, 0);
+
+  return WEXITSTATUS(status1) && WEXITSTATUS(status2);
 }
 
 /**
@@ -225,19 +263,20 @@ int parse_command(command_t *c, int level, command_t *father) {
   switch (c->op) {
   case OP_SEQUENTIAL:
     /* TODO: Execute the commands one after the other. */
-	 status = parse_command(c->cmd1, level + 1, c);
-	 return parse_command(c->cmd2, level + 1, c);	
+    status = parse_command(c->cmd1, level + 1, c);
+    return parse_command(c->cmd2, level + 1, c);
     break;
 
   case OP_PARALLEL:
     /* TODO: Execute the commands simultaneously. */
+    return run_in_parallel(c->cmd1, c->cmd2, level, c);
     break;
 
   case OP_CONDITIONAL_NZERO:
     /* TODO: Execute the second command only if the first one
      * returns non zero.
      */
-	 status = parse_command(c->cmd1, level + 1, c);
+    status = parse_command(c->cmd1, level + 1, c);
     if (status != 0)
       return parse_command(c->cmd2, level + 1, c);
     return status;
@@ -247,7 +286,7 @@ int parse_command(command_t *c, int level, command_t *father) {
     /* TODO: Execute the second command only if the first one
      * returns zero.
      */
-	 status = parse_command(c->cmd1, level + 1, c);
+    status = parse_command(c->cmd1, level + 1, c);
     if (status == 0)
       return parse_command(c->cmd2, level + 1, c);
     return status;
